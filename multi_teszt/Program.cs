aeaea3385d36a3c54c10d5143bfa585612d2699b
Molnár Attila <molnar.attila@szlgbp.hu>
2022-07-18 12:33:41 +0200

    Step-by-step debuggal működik, mert az lassú. Valahol összeakadnak a szálak. Megnézem a Monitor classt, azt írják, ez a suspend amúgyis elavult.

--- multi_teszt/Program.cs
+++ multi_teszt/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace multi_teszt
 {
@@ -14,10 +14,15 @@ namespace multi_teszt
 			Jatekos lilesz = new Jatekos("Lilesz");
 			Metronom m = new Metronom();
 
-			karesz.Folyamat(10);
-			lilesz.Folyamat(15);
+			foreach (Jatekos jatekos in Jatekos.lista)
+			{
+				jatekos.thread.Start();
+			}
+
 			m.Start(20);
 		}
+		static void Karesz_folyamata() => Jatekos.lista[0].Folyamat();
+		static void Lilesz_folyamata() => Jatekos.lista[1].Folyamat();
 	}
 
 	class Metronom
@@ -27,7 +32,8 @@ namespace multi_teszt
 		public void UjKor()
 		{
 			foreach (Jatekos jatekos in Jatekos.lista)
-				jatekos.Indit();
+				if (!jatekos.kész)
+					jatekos.Ujraindit();
 			Console.WriteLine($"{szamlalo++}. kör vége");
 		}
 
@@ -40,34 +46,53 @@ namespace multi_teszt
 
 	class Jatekos
 	{
+		// instancia tulajdonságai
 		public string nev;
-		
+		public Thread thread;
+		private int meddig;
+		public bool kész;
+		public bool vár;
+		// osztály tulajdonságai
 		public static List<Jatekos> lista = new List<Jatekos>();
+		
+		public static Random r = new Random();
 
+		// konstruktor
 		public Jatekos(string nev)
 		{
 			this.nev = nev;
+			this.thread = new Thread(new ThreadStart(Folyamat));
+			this.meddig = r.Next(10,20);
+
 			lista.Add(this);
+			kész = false;
+
+			Console.WriteLine($"{nev} létrejött, és {meddig}-ig fog elszámolni.");
 		}
 
-		public void Folyamat(int meddig)
+		// metódusok
+		public void Folyamat()
 		{
 			for (int i = 0; i < meddig; i++)
 				Lep();
+
+			kész = true;
 		}
 
-		public void Indit() 
-		{ 
-			
+		public void Ujraindit() 
+		{
+			thread.Resume();
 		}
 		public void Megall() 
-		{ 
-
+		{
+			thread.Suspend();
 		}
 		public void Lep()
 		{
 			Console.WriteLine($"{nev} lép.");
 			Megall();
+			vár = true;
+			
 		}
 	}
 }
